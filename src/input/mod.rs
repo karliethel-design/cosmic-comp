469893b1692b169bb790097c32eee309c8630e67
karliethel-design <karliethel@gmail.com>
2026-04-28 12:47:26 -0600

    updated the order to check if the pointer is locked first, then if locked to send relative_motion, then return, if not locked fall through to the normal relative_motion+position update path as before

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -370,6 +370,19 @@ impl State {
                         .map(|(target, pos)| (target, pos.as_logical()));
 
                     std::mem::drop(shell);
+                    if pointer_locked {
+                        ptr.relative_motion(
+                            self,
+                            under.clone(),
+                            &RelativeMotionEvent {
+                                delta: event.delta(),
+                                delta_unaccel: event.delta_unaccel(),
+                                utime: event.time(),
+                            },
+                        );
+                        ptr.frame(self);
+                        return;
+                    }
                     ptr.relative_motion(
                         self,
                         under.clone(),
@@ -379,11 +392,6 @@ impl State {
                             utime: event.time(),
                         },
                     );
-
-                    if pointer_locked {
-                        ptr.frame(self);
-                        return;
-                    }
 
                     if ptr.is_grabbed() {
                         if seat
